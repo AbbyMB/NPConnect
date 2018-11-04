8e0b87c3e059634b1742c55ca6aaf71b4bdc3c06
Abby MB <abbymbardin@gmail.com>
2018-11-03 22:25:41 -0400

    create search funder function using filter.

--- app/javascript/react/containers/FundersContainer.js
+++ app/javascript/react/containers/FundersContainer.js
@@ -6,8 +6,13 @@ class FundersContainer extends Component {
   constructor(props){
     super(props);
     this.state = {
-      funders: []
+      funders: [],
+      searchedFunders: [],
+      searchedCategory: 'select category'
     }
+  this.handleSearchSubmit = this.handleSearchSubmit.bind(this)
+  this.handleCategoryChange = this.handleCategoryChange.bind(this)
+  this.handleReset = this.handleReset.bind(this)
   }
 
   componentDidMount(){
@@ -28,8 +33,41 @@ class FundersContainer extends Component {
       .catch(error => console.error(`Error in fetch: ${error.message}`));
   }
 
-  render(){
+  handleCategoryChange(event){
+    this.setState({ searchedCategory: event.target.value })
+  }
 
+  handleSearchSubmit(event){
+    event.preventDefault()
+    let searchedCategory = this.state.searchedCategory
+    let funders = this.state.funders
+    let searchedFunders = funders.filter((funder) => {
+      if (funder.category === searchedCategory){
+        return funder
+      }
+    });
+    this.setState({ funders: searchedFunders })
+  }
+  handleReset(event){
+    this.setState({ searchedFunders: [], searchedCategory: 'select category' })
+    fetch(`/api/v1/funders`)
+      .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          let errorMessage = `${response.status} (${response.statusText})`,
+              error = new Error(errorMessage);
+          throw(error);
+        }
+      })
+      .then(response => response.json())
+      .then(body => {
+        this.setState({ funders: body.funders });
+      })
+      .catch(error => console.error(`Error in fetch: ${error.message}`));
+  }
+
+  render(){
     let funders = this.state.funders.map(funder => {
       return(
         <FunderTile
@@ -42,11 +80,37 @@ class FundersContainer extends Component {
         />
       )
     })
+
     return(
       <div>
         <div className="row" id="funderText">
           <div className="small-8 small-centered columns">
             <h3>Funders:</h3>
+            <h5>Search Funders By Category</h5>
+            <form onSubmit={this.handleSearchSubmit}>
+            <label>
+              Search by Category:
+              <select onChange={this.handleCategoryChange} value={this.state.searchedCategory}>
+                <option value="select category">Select category</option>
+                <option value="education">education</option>
+                <option value="child health">child health</option>
+                <option value="child welfare">child welfare</option>
+                <option value="food assistance">food assistance</option>
+                <option value="elder services">elder services</option>
+                <option value="transportation">transportation</option>
+                <option value="social justice">social justice</option>
+                <option value="housing">housing</option>
+                <option value="environmental">environmental</option>
+                <option value="youth development">youth development</option>
+              </select>
+            </label>
+              <button className="button">
+                Search
+              </button>
+              <button onClick={this.handleReset} className="button">
+                Reset
+              </button>
+            </form>
             {funders}
           </div>
         </div>
