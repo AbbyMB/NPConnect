59240cd224b3d6b3e36a55bea9a2797cbcf97e2d
Abby MB <abbymbardin@gmail.com>
2018-11-11 20:48:34 -0500

    edit spec

--- spec/models/partnership_spec.rb
+++ spec/models/partnership_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe Partnership, type: :model do
   describe "valid partnership" do
     let!(:user) { User.create(email: "bob@bob.com", password: "888888", username: "bobG100", first_name: "Bob", last_name: "Green") }
-    let!(:program) { Program.create(name: "Project Help", description: "Provides help and support", category: "Child Welfare", owner_id: user.id, address: "330 Homer Street Newton, MA 02459") }
+    let!(:program) { Program.create(name: "Project Help", description: "Provides help and support", category: "Child Welfare", owner_id: user.id, address: "330 Homer Street Newton, MA 02459", lat: 133333333, lng: 11111122222) }
 
     it "Is valid with attributes" do
-      partnership_new = Partnership.new(user_id: user.id, program_id: 1)
+      partnership_new = Partnership.new(user_id: user.id, program_id: program.id)
       expect(partnership_new).to be_valid
     end
 
     it "Is not valid without a user id" do
-      no_user = Partnership.new(program_id: 1)
+      no_user = Partnership.new(program_id: program.id)
       expect(no_user).to_not be_valid
     end
 
