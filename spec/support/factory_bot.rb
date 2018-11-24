c165073459d7156688feacbe0c7fd9eea1aa1869
Abby MB <abbymbardin@gmail.com>
2018-11-23 21:21:27 -0500

    Finish capybara feature tests

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -5,6 +5,9 @@ FactoryBot.define do
     sequence(:email) {|n| "user#{n}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
+    first_name { 'first_name' }
+    last_name { 'last_name' }
+    username { 'username' }
   end
 
 end
